fc66af30204bf9a5b5761537c32bf23474a0034e
hiren timbadiya <hirentimbadiya74@gmail.com>
2024-04-23 11:24:13 +0530

    used jsx
    Signed-off-by: hiren timbadiya <hirentimbadiya74@gmail.com>

---
+++ client/src/Components/Notes.jsx
@@ -0,0 +1,88 @@
+import React, { useContext, useEffect, useState, useRef } from "react";
+import noteContext from "../context/notes/NoteContext";
+import Noteitem from "./Noteitem";
+import AddNote from "./AddNote";
+import { useNavigate } from "react-router-dom";
+
+const Notes = (props) => {
+  const context = useContext(noteContext);
+  const { memoizedNotes, getNotes, editNote } = context;
+  let navigate = useNavigate();
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      getNotes();
+    }
+    else {
+      navigate('/login');
+      props.showAlert("Please Login First to access your Notes", "danger")
+    }
+  }, [])
+
+  const refOpen = useRef(null)
+  const refClose = useRef(null)
+  const [note, setNote] = useState({ id: "", etitle: "", edescription: "", etag: "" })
+
+  const updateNote = (currentNote) => {
+    refOpen.current.click();
+    setNote({ id: currentNote._id, etitle: currentNote.title, edescription: currentNote.description, etag: currentNote.tag })
+  }
+
+  const handleClick = (e) => {
+    editNote(note.id, note.etitle, note.edescription, note.etag)
+    refClose.current.click();
+  }
+
+  const onChange = (e) => {
+    setNote({ ...note, [e.target.name]: e.target.value })
+  }
+  return (
+    <>
+      <AddNote />
+      <button ref={refOpen} type="button" className="btn btn-primary d-none" data-bs-toggle="modal" data-bs-target="#exampleModal">
+        Launch demo modal
+      </button>
+      <div className="modal fade" id="exampleModal" tabIndex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h5 className="modal-title" id="exampleModalLabel">Edit Note</h5>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div className="modal-body">
+              <form className="my-3">
+                <div className="mb-3">
+                  <label htmlFor="title" className="form-label">Title</label>
+                  <input type="text" className="form-control" id="etitle" name="etitle" value={note.etitle} aria-describedby="emailHelp" onChange={onChange} required minLength={3} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="description" className="form-label">Description</label>
+                  <input type="text" className="form-control" id="edescription" name="edescription" value={note.edescription} onChange={onChange} required minLength={5} />
+                </div>
+                <div className="mb-3">
+                  <label htmlFor="tag" className="form-label">Tag</label>
+                  <input type="text" className="form-control" id="etag" name="etag" value={note.etag} onChange={onChange} required />
+                </div>
+
+              </form>
+            </div>
+            <div className="modal-footer">
+              <button type="button" ref={refClose} className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+              <button onClick={handleClick} type="button" className="btn btn-primary">Update Note</button>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="row">
+        <h3 className="mt-4">Your Notes</h3>
+        <div className="container">
+          {memoizedNotes.length === 0 && "No Notes to display"}
+        </div>
+        {memoizedNotes.map((note) => {
+          return <Noteitem key={note._id} note={note} updateNote={updateNote}></Noteitem>;
+        })}
+      </div>
+    </>
+  );
+};
+
+export default Notes;
--- client/src/context/notes/NoteContext.js
+++
@@ -1,5 +0,0 @@
-const { createContext } = require("react");
-
-const noteContext = createContext();
-
-export default noteContext;
